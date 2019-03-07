8e8bfcdaa62e2b65b4693246c4ef0bb517741d40
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-03-07 10:13:54 +0300

    init .net core mvc

---
+++ CoreGiris/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreGiris.Models;
+
+namespace CoreGiris.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Kisi> kisiler = new List<Kisi>()
+        {
+            new Kisi()
+            {
+                Ad = "Kamil",
+                Soyad = "Falan"
+            },
+            new Kisi()
+            {
+                Ad = "Hakkı",
+                Soyad = "Filan"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            ViewBag.Hello = "Hello World";
+            return View(kisiler);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoreGiris/Models/Kisi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreGiris.Models
+{
+    public class Kisi
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+}
