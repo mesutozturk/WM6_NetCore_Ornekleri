39ae5ca6c67be1d6097f8276dbc92fb83c6875cb
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-03-08 10:52:40 +0300

    remove

--- CoreIdentity/Models/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoreIdentity.Models
-{
-    public class LoginViewModel
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public bool RememberMe { get; set; }
-    }
-}
