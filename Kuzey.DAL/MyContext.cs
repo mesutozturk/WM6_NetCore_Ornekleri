37a38a69dff03f59eb47cf73a9e21705873cfbb7
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-03-18 09:59:18 +0300

    mycontext

---
+++ Kuzey.DAL/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kuzey.DAL
+{
+    public class MyContext : IdentityDbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+        : base(options)
+        {
+        }
+    }
+}
+
